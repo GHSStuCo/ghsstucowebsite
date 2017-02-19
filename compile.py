70be88e93d1369d1eb6098f393a378d6058a7d36
Daniel Rebelsky <drebelsky@gmail.com>
2017-02-19 12:50:42 -0600

    Added basic if else functionality to templating

--- compile.py
+++ compile.py
@@ -14,6 +14,18 @@ def template(file):
     template_data = template_file.read()
     for i in vars.keys():
       template_data = template_data.replace("{"+i+"}",vars[i])
+    while(template_data.find("{if") != -1):
+      t_start = template_data.find("{if")
+      t_end = template_data.find("}",t_start)
+      test = template_data[template_data.find("(", t_start)+1 : template_data.find(")", t_start)]
+      name, value = test.split("==")
+      if (vars[name] == value):
+        replace_start = template_data.find("[", t_start)
+        replace_end = template_data.find("]", t_start)
+      else:
+        replace_start = template_data.find("[", template_data.find("]", t_start))
+        replace_end = template_data.find("]", replace_start)
+      template_data = template_data[:t_start] + template_data[replace_start+1:replace_end] + template_data[t_end+1:]
     file = file[:start]+template_data+file[end+11:]
   return file
 
